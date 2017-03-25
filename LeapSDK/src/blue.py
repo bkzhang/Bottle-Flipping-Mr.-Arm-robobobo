df53af03a240840a8fe847bff7019331e2875557
Henry Wang <h397wang@uwaterloo.ca>
2017-03-25 15:10:22 -0400

    added sample, and progress on blue.py

--- LeapSDK/src/blue.py
+++ LeapSDK/src/blue.py
@@ -1,12 +1,20 @@
 import bluetooth
 
-nearby_devices = bluetooth.discover_devices(lookup_names=True)
-print "found %d devices" % len(nearby_devices)
+target_name = "HC-05"
+target_address = None
 
-for addr, name in nearby_devices:
-	print "%s - %s" % (addr, name)
+def connect():
+    print("Discovering nearby devices")
+    nearby_devices = bluetooth.discover_devices()
+    for device in nearby_devices:
+        if target_name == bluetooth.lookup_name(device)
+            target_address = device
+            break
+    if target_address is not None:
+        print("Found target bluetooth device with address ")
+        print(target_address)
 
-def send_arduino():
+def send_arduino(string):
 	print("Sending signals to the Arduino")
 
 
