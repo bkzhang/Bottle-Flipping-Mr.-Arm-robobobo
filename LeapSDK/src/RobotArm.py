2ba0c92a1ba99a681006e1aea8c24665412c1b07
Henry Wang <h397wang@uwaterloo.ca>
2017-03-25 19:58:50 -0400

    added RobotArm.py

---
+++ LeapSDK/src/RobotArm.py
@@ -0,0 +1,33 @@
+
+# this file uses tabs instead of spaces
+class RobotArm():
+	is_claw_open = True
+	arm_angle = 45 # ranges from 0 to 90 degrees
+	base_rotation_angle = 0 # range from -45 to 45?
+
+	def reset(self):
+		is_claw_open = True
+		arm_angle = 45 # ranges from 0 to 90 degrees
+		base_rotation_angle = 0
+
+	def __init__(self):
+		self.reset()
+
+	def update(self, _is_claw_open, _arm_angle, _base_rotation_angle):
+		
+		is_claw_open = _is_claw_open
+
+		if _arm_angle > 90:
+			arm_angle = 90
+		else if _arm_angle < 0:
+			arm_angle = 0
+		else:	
+			arm_angle = _arm_angle 
+
+		if _base_rotation_angle > 45:
+			base_rotation_agle = 45
+		else if _base_rotation_angle < -45
+			base_rotation_agle = -45
+		else:
+			base_rotation_angle = _base_rotation_angle
+
