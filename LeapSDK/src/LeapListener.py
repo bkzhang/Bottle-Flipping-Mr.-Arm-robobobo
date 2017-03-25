94348541d2b7be90d8be1874d6ed889b7ed564eb
Henry Wang <h397wang@uwaterloo.ca>
2017-03-25 17:17:58 -0400

    test

--- LeapSDK/src/LeapListener.py
+++ LeapSDK/src/LeapListener.py
@@ -47,42 +47,41 @@ class LeapListener(Leap.Listener):
         # Get the most recent frame and report some basic information
         frame = controller.frame()
 
-        if PRINT == 1:
+        if 0:
             print "Frame id: %d, timestamp: %d, hands: %d, fingers: %d, tools: %d, gestures: %d" % (frame.id, frame.timestamp, len(frame.hands), len(frame.fingers), len(frame.tools), len(frame.gestures()))
 
         # Get hands
         for hand in frame.hands:
 
-            handType = "Left hand" if hand.is_left else "Right hand"
+            if hand.is_left:
+                break
 
-            print "  %s, id %d, position: %s" % (
-                handType, hand.id, hand.palm_position)
+            print "Right palm position: %s" % (hand.palm_position)
 
             # Get the hand's normal vector and direction
             normal = hand.palm_normal
             direction = hand.direction
 
+            if 0:
             # Calculate the hand's pitch, roll, and yaw angles
-            print "  pitch: %f degrees, roll: %f degrees, yaw: %f degrees" % (
-                direction.pitch * Leap.RAD_TO_DEG,
-                normal.roll * Leap.RAD_TO_DEG,
-                direction.yaw * Leap.RAD_TO_DEG)
+                print "  pitch: %f degrees, roll: %f degrees, yaw: %f degrees" % (direction.pitch * Leap.RAD_TO_DEG, normal.roll * Leap.RAD_TO_DEG, direction.yaw * Leap.RAD_TO_DEG)
 
             # Get arm bone
             arm = hand.arm
-            print "  Arm direction: %s, wrist position: %s, elbow position: %s" % (
-                arm.direction,
-                arm.wrist_position,
-                arm.elbow_position)
+            if 0:
+                print "  Arm direction: %s, wrist position: %s, elbow position: %s" % (
+                    arm.direction,
+                    arm.wrist_position,
+                    arm.elbow_position)
 
             # Get fingers
             for finger in hand.fingers:
-
-                print "    %s finger, id: %d, length: %fmm, width: %fmm" % (
-                    self.finger_names[finger.type],
-                    finger.id,
-                    finger.length,
-                    finger.width)
+                if 0:
+                    print "    %s finger, id: %d, length: %fmm, width: %fmm" % (
+                        self.finger_names[finger.type],
+                        finger.id,
+                        finger.length,
+                        finger.width)
 
                 # Get bones
                 for b in range(0, 4):
