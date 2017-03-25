8490de8b513f65d0c808c2901e1fafc43d01859e
bkzhang <brian.k.zhang@gmail.com>
2017-03-25 12:25:10 -0400

    should be the basis of connecting to leap motion

--- robot.py
+++ robot.py
@@ -1,19 +1,31 @@
 import os, sys
+import Leap
+from Leap import CircleGesture, KeyTapGesture, ScreenTapGesture, SwipeGesture
 
 src_dir = os.path.dirname(inspect.getfile(inspect.currentframe()))
 sys.path.insert(0, os.path.abspath(os.path.join(src_dir, '../lib')))
 sys.path.insert(0, os.path.abspath(os.path.join(src_dir, '../lib/x64')))
 
-import Leap
+class Listener(Leap.Listener):
+    def on_connect(self, controller):
+        print "Connected"
+    
+    def on_frame(self, controller):
+        print "Frame available"
 
 def controller():
+    listener = Listener()
     controller = Leap.Controller()
+
+    controller.add_listener(listener)
 
     print "Press any key to quit"
     try:
         sys.stdin.readline()
     except KeyboardInterrupt:
         pass
+    finally:
+        contoller.remove_listener(listener)
 
 if __name__ == '__main__':
     connect()
